b575b8072c3f2859d567a99e68bd0864917c4507
kouata <sande7kouata@gmail.com>
2020-01-24 16:14:05 +0100

    tdd, after adding feature function

--- app.py
+++ app.py
@@ -11,5 +11,9 @@ def hello_world():
 def hello_user(username):
     return 'Hello %s!\n' % username
 
+@app.route('/feature/<string>') # dynamic route
+def hello_user(string):
+    return 'Hello %s!\n' % sting
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0') # open for everyone
